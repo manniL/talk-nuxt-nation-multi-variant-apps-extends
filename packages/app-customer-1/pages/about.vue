36264bbfaca359bc85213005553dbeb7e2f3717c
Alexander Lichter <github@lichter.io>
2022-11-15 14:04:46 +0100

    wip

--- packages/app-customer-1/pages/about.vue
+++ packages/app-customer-1/pages/about.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+useHead({
+  script: [
+    {
+      src: '/js/main.js',
+      type: 'text/javascript',
+      body: true,
+    },
+  ],
+});
+</script>
+
 <template>
   <h1>New about page</h1>
 </template>
